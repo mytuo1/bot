4ebce3f79368310163f893a4a73d4efe1ac353c8
root <root@instance-1.us-west4-b.c.eng-blade-338223.internal>
2022-01-17 23:49:09 +0000

    first commit added folderless lists

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  TOKEN: "OTMxNDgzMTk0NjkwMjU2ODk3.YeFFUw.xuylPFBSdLKT1MYNxXvHMKSZSRY",
+  PREFIX: "cu!",
+  STAFF_ROLE: "Staff",
+  CLICKUP_TOKEN: "pk_36506021_BM3Q1TI255TAHF7IC1R1A6TBCNYZ438Q", // you could (and probably should) make a "dummy" account for the bot
+  TEAM_ID: "1255586",
+  SPACE_ID: "6703404",
+};
