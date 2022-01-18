2c2c6f0fb5704599edadd309bddd35504d61e087
mytuo1 <mutio89@gmail.com>
2022-01-18 04:30:30 +0200

    changes to code

--- commands/statuses.js
+++ commands/statuses.js
@@ -4,7 +4,13 @@ const config = require("../config");
 
 exports.run = async (client, message, args) => {
 
-    const status = await ClickUpAPIUtils.getStatus(args[0]);
+    const newArgs = message.content
+        .slice(config.PREFIX.length + this.help.name.length)
+        .trim()
+        .split("|")
+        .map((x) => x.trim());
+
+    const status = await ClickUpAPIUtils.getStatus(newArgs[0]);
     if (status.err) return message.channel.send(`Error fetching task status: ${status.err}`);
 
     return message.channel.send(`The status of this task is :\`\`${status}\`\`\``);
