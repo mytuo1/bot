ea68769614b567ca2868b6a280895ae1e58476d5
mytuo1 <mutio89@gmail.com>
2022-01-18 05:54:38 +0200

    changes to code

--- events/message.js
+++ events/message.js
@@ -8,8 +8,9 @@ module.exports = async (client, message) => {
 
   const args = message.content.slice(config.PREFIX.length).trim().split(/ +/g);
   const cmdName = args.shift().toLowerCase();
+  return message.channel.send("cmd is " ${cmdName})
   const cmd1 = cmdName /*client.commands.find((x) => x.help.name === cmdName);*/
-  const cmd = client.commands.find((x) => x.name === cmdName);
+  const cmd = client.commands.find((x) => x.help.has(cmdName));
 
   if (!cmd) return;
 /*  console.log(
