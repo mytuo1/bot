0c9aa334937a785f6df93aae153b8bb45b883d7e
mytuo1 <mutio89@gmail.com>
2022-01-18 04:56:25 +0200

    changes to code

--- commands/newtask.js
+++ commands/newtask.js
@@ -4,26 +4,26 @@ const ClickUpAPIUtils = require("../ClickUpAPIUtils");
 
 exports.run = async (client, message, args) => {
   const newArgs = message.content
-    .slice(config.PREFIX.length + this.help.name.length)
-    .trim()
-    .split("|")
-    .map((x) => x.trim());
+      .slice(config.PREFIX.length + this.help.name.length)
+      .trim()
+      .split("|")
+      .map((x) => x.trim());
 
-     if (newArgs.length !== 3)
+  if (newArgs.length !== 3)
     return message.channel.send(
-      "Please add more arguments with a \`\`|\`\` delimiter."
+        "You don't give me information, i cant file it. stop wasting my time >:("
     );
-    const folders = await ClickUpAPIUtils.getFLists(); 
-    const list = folders.find((x) => x.name.toLowerCase() === newArgs[2] || x.id === newArgs[2]);
+  const folders = await ClickUpAPIUtils.getFLists();
+  const list = folders.find((x) => x.name.toLowerCase() === newArgs[2] || x.id === newArgs[2]);
 
   if (!list) return message.channel.send(`invalid list`);
-  else if (list.err) return message.channel.send(`Error getting list: ${list.err}`);
+  else if (list.err) return message.channel.send(`error getting list: ${list.err}`);
 
   const res = await ClickUpAPIUtils.createTask(list.id, newArgs[0], newArgs[1]);
-  if (res.err) return message.channel.send(`Error on task creation! ${res.err}`);
+  if (res.err) return message.channel.send(`fdgffrg! ${res.err}`);
 
   return message.channel.send(
-    `Created a new task under the list \`\`${list.name}\`\`; Task link: https://app.clickup.com/t/${res.id}`
+      `Created a new task under the list \`\`${list.name}\`\`; Task id: \`\`${res.id}\`\``
   );
 };
 
