ef6fbdf8cbb247fcc39bbe63c867ce1d3e42f163
mytuo1 <mutio89@gmail.com>
2022-01-18 04:09:32 +0200

    changes to code

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  TOKEN: "OTMxNDgzMTk0NjkwMjU2ODk3.YeFFUw.xuylPFBSdLKT1MYNxXvHMKSZSRY",
+  TOKEN: "OTMyNzg0NjA3ODQwMDY3NjU0.YeYBXA.xnTeYvZqbNE5EoaISnjknNXwIJY",
   PREFIX: "cu!",
   STAFF_ROLE: "Staff",
   CLICKUP_TOKEN: "pk_36506021_BM3Q1TI255TAHF7IC1R1A6TBCNYZ438Q", // you could (and probably should) make a "dummy" account for the bot
