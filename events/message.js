e9386ac68860fff94aeacdc52a7f40b6a0c8bfbf
mytuo1 <mutio89@gmail.com>
2022-01-18 06:26:15 +0200

    changes to code

--- events/message.js
+++ events/message.js
@@ -8,8 +8,8 @@ module.exports = async (client, message) => {
 
   const args = message.content.slice(config.PREFIX.length).trim().split(/ +/g);
   const cmdName = args.shift().toLowerCase();
-  return message.channel.send(client.commands.print().toString());
-  const cmd = client.commands.find((x) => x.help.name === cmdName);
+  return message.channel.send(client.commands.find(x => x.help.name === cmdName));
+  const cmd = client.commands.find(x => x.help.name === cmdName);
   if (!cmd) return;
   console.log(
       `[Comamnd Execution] ${message.author.username} (${message.author.id}) ran command ${
